f4f07d524dd648e4031eeee89d19c2911dc85fc8
Mridula S <mridula26022006@gmail.com>
2025-12-23 23:33:19 +0530

    Time: 16 ms (14.57%), Space: 47.2 MB (12.41%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]","");
+        StringBuilder rev = new StringBuilder();
+        for(int i=s.length()-1;i>=0;i--){
+            rev.append(s.charAt(i));
+        }
+        return rev.toString().equals(s);
+    }
+}
