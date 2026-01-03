932024cc91c7caca5fe3cc404099d35490174b73
Mridula S <mridula26022006@gmail.com>
2026-01-03 14:34:25 +0530

    Time: 1 ms (99.84%), Space: 47 MB (26.85%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String>re = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+        if(i%3==0 && i%5==0){
+            re.add("FizzBuzz");
+        }
+        else if(i% 3 == 0){
+            re.add("Fizz");
+        }
+        else if(i % 5 == 0){
+            re.add("Buzz");
+        }
+        else{
+            re.add(String.valueOf(i));
+        }
+        }
+        return re;
+    }
+};
