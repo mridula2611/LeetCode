47d061fbc1f7eb501086a77af784905181fd652d
Mridula S <mridula26022006@gmail.com>
2026-01-19 11:49:40 +0530

    Time: 0 ms (100%), Space: 42 MB (63.7%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int integerBreak(int n) {
+        if(n<=3) return n-1;
+        int result = 1;
+        while(n>4){
+            result = result * 3;
+            n = n-3;
+        }
+        return result * n;
+    }
+}
